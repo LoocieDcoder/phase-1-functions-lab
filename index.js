425ef1e4f6dfcf469352cc5d6d5a0deaced2b520
Lucy <quaysonlucyofficial@gmail.com>
2022-05-13 06:51:48 +0100

    Assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(blocks) {
+    return Math.abs(blocks-42);
+};
+
+
+function distanceFromHqInFeet(block){
+    return (Math.abs(block-42)*264);
+  }
+  
+  function distanceTravelledInFeet(dis1,dis2){
+    return (Math.abs(dis2-dis1)*264)
+  
+  }
+  
+  function calculatesFarePrice(start, destination){
+    if(distanceTravelledInFeet(start,destination) < 400){
+      return 0;
+    } else if(distanceTravelledInFeet(start,destination) >= 400 && distanceTravelledInFeet(start,destination) <= 2000){
